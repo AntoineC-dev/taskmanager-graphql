694847705cc6d4d3556a62c9647e8d1629424da6
AntoineC-dev <ac.dev@hotmail.com>
2022-02-19 17:31:39 -0400

    Create nodemailer utils

--- server/src/utils/index.ts
+++ server/src/utils/index.ts
@@ -1,3 +1,4 @@
 export { JWTPayload, deserializeTokens, signTokens } from "./jwt.utils";
 export { default as logger } from "./logger.utils";
+export { sendVerificationEmail, verifySMTP } from "./mailer.utils";
 export { comparePwd, hashPwd } from "./pwd.utils";
