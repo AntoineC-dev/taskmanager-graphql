610ebffbc566ee0fcddb46330cb5e263932f9663
AntoineC-dev <ac.dev@hotmail.com>
2022-02-19 15:59:01 -0400

    Add smtp config

--- server/config/default.ts
+++ server/config/default.ts
@@ -7,4 +7,13 @@ export default {
   refreshTokenTtl: "1y",
   publicKey: process.env.PUBLIC_KEY,
   privateKey: process.env.PRIVATE_KEY,
+  smtp: {
+    host: process.env.SMTP_HOST,
+    port: process.env.SMTP_PORT,
+    from: process.env.SMTP_USER,
+    auth: {
+      user: process.env.SMTP_USER,
+      pass: process.env.SMTP_PASSWORD,
+    },
+  },
 };
