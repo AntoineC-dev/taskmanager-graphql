f2f5497705419efbf7e484709eb61c0140c09cbc
AntoineC-dev <ac.dev@hotmail.com>
2022-02-19 17:29:53 -0400

    Add SMTP check

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,7 +2,7 @@ import { ApolloServer } from "apollo-server";
 import config from "config";
 import { context } from "./context";
 import { schema } from "./schema";
-import { logger } from "./utils";
+import { logger, verifySMTP } from "./utils";
 
 export const server = new ApolloServer({
   schema,
@@ -13,4 +13,5 @@ const port = config.get<string>("port");
 
 server.listen({ port }).then(({ url }) => {
   logger.info(`Server listening at ${url}`);
+  verifySMTP();
 });
