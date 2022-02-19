f18231a3b6deff5b094cd07fdfc0c57236b0734a
AntoineC-dev <ac.dev@hotmail.com>
2022-02-19 17:29:13 -0400

    Create nodemailer utils

---
+++ server/src/utils/mailer.utils.ts
@@ -0,0 +1,52 @@
+import nodemailer from "nodemailer";
+import config from "config";
+import logger from "./logger.utils";
+import { User } from "@prisma/client";
+
+interface SMTPConfig {
+  host: string;
+  port: number;
+  from: string;
+  auth: {
+    user: string;
+    pass: string;
+  };
+}
+
+const smtp = config.get<SMTPConfig>("smtp");
+
+const transporter = nodemailer.createTransport({
+  pool: true,
+  secure: true, // Only with port 465
+  ...smtp,
+});
+
+export function verifySMTP() {
+  transporter.verify((error, _) => {
+    if (error) {
+      logger.error(error, "SMTP Error");
+      process.exit(1);
+    }
+    logger.info("Connected to SMTP");
+  });
+}
+
+export function sendVerificationEmail(user: User) {
+  const { id, email, verificationCode } = user;
+  transporter.sendMail(
+    {
+      from: `Graphql TaskManager<${smtp.from}>`,
+      to: email,
+      subject: "Please verify your account",
+      text: `id:${id} -- verificationCode:${verificationCode}`,
+    },
+    (error, _) => {
+      if (error) {
+        logger.error(error, "VerificationEmail Error");
+        return false;
+      }
+      logger.info(`Verification email sent to ${email}`);
+      return true;
+    }
+  );
+}
