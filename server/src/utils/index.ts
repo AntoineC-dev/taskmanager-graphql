0ba222378c922625ff11a8eaa9f5c628d3980700
AntoineC-dev <ac.dev@hotmail.com>
2022-02-20 00:21:17 -0400

    Update uuid utils

--- server/src/utils/index.ts
+++ server/src/utils/index.ts
@@ -10,3 +10,4 @@ export { JWTPayload, deserializeTokens, signTokens } from "./jwt.utils";
 export { default as logger } from "./logger.utils";
 export { sendVerificationEmail, verifySMTP } from "./mailer.utils";
 export { comparePwd, hashPwd } from "./pwd.utils";
+export { generateUniqueIdentifier } from "./uuid.utils";
