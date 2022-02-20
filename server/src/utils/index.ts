329ac9cbff7b6be415208638b7fbf1582102c1d3
AntoineC-dev <ac.dev@hotmail.com>
2022-02-20 00:24:20 -0400

    Export sendPasswordResetCodeEmail from utils

--- server/src/utils/index.ts
+++ server/src/utils/index.ts
@@ -8,6 +8,6 @@ export {
 } from "./errors.utils";
 export { JWTPayload, deserializeTokens, signTokens } from "./jwt.utils";
 export { default as logger } from "./logger.utils";
-export { sendVerificationEmail, verifySMTP } from "./mailer.utils";
+export { sendPasswordResetCodeEmail, sendVerificationEmail, verifySMTP } from "./mailer.utils";
 export { comparePwd, hashPwd } from "./pwd.utils";
 export { generateUniqueIdentifier } from "./uuid.utils";
