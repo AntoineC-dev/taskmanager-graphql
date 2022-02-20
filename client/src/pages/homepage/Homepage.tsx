4ac394c552390044d883344b802b61426abd284d
AntoineC-dev <ac.dev@hotmail.com>
2022-02-20 17:50:09 -0400

    Extract Homepage from App

---
+++ client/src/pages/homepage/Homepage.tsx
@@ -0,0 +1,21 @@
+import { Box, Code, Grid, Link, Text, VStack } from "@chakra-ui/react";
+import { ColorModeSwitcher, Logo } from "../../components";
+
+export const Homepage = () => {
+  return (
+    <Box textAlign="center" fontSize="xl">
+      <Grid minH="100vh" p={3}>
+        <ColorModeSwitcher justifySelf="flex-end" />
+        <VStack spacing={8}>
+          <Logo h="40vmin" pointerEvents="none" />
+          <Text>
+            Edit <Code fontSize="xl">src/App.tsx</Code> and save to reload.
+          </Text>
+          <Link color="teal.500" href="https://chakra-ui.com" fontSize="2xl" target="_blank" rel="noopener noreferrer">
+            Learn Chakra
+          </Link>
+        </VStack>
+      </Grid>
+    </Box>
+  );
+};
