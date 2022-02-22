20b71c6a89ea9cc6e13f0ae57cbffa7e09a83c61
AntoineC-dev <ac.dev@hotmail.com>
2022-02-21 23:04:13 -0400

    Add authMiddleware & logoutMiddleware

--- client/src/graphql/client.ts
+++ client/src/graphql/client.ts
@@ -1,6 +1,27 @@
-import { ApolloClient, InMemoryCache } from "@apollo/client";
+import { ApolloClient, ApolloLink, concat, HttpLink, InMemoryCache } from "@apollo/client";
+import { onError } from "@apollo/client/link/error";
+
+const httpLink = new HttpLink({
+  uri: "http://localhost:3001",
+  credentials: "include",
+});
+const authMiddleware = new ApolloLink((operation, forward) => {
+  operation.setContext(({ headers = {} }) => ({
+    headers: {
+      ...headers,
+      authorization: localStorage.getItem("token") || null,
+    },
+  }));
+  return forward(operation);
+});
+
+const logoutLink = onError(({ response }) => {
+  if (response?.extensions && response.extensions.code === "FORBIDDEN" && localStorage.getItem("token")) {
+    localStorage.removeItem("token");
+  }
+});
 
 export const client = new ApolloClient({
-  uri: "http://localhost:3001",
   cache: new InMemoryCache(),
+  link: concat(authMiddleware, logoutLink.concat(httpLink)),
 });
