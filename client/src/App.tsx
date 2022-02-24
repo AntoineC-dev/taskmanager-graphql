d06f91556084897197643ecbbddb265653a31a37
AntoineC-dev <ac.dev@hotmail.com>
2022-02-24 14:20:04 -0400

    Switch ApolloProvider from index to App

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,12 @@
+import { ApolloProvider } from "@apollo/client";
 import { ChakraProvider, theme } from "@chakra-ui/react";
+import { client } from "./graphql";
 import { Router } from "./navigation";
 
 export const App = () => (
   <ChakraProvider theme={theme}>
-    <Router />
+    <ApolloProvider client={client}>
+      <Router />
+    </ApolloProvider>
   </ChakraProvider>
 );
--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,16 +1,12 @@
-import { ApolloProvider } from "@apollo/client";
 import { ColorModeScript } from "@chakra-ui/react";
 import * as React from "react";
 import ReactDOM from "react-dom";
 import { App } from "./App";
-import { client } from "./graphql";
 
 ReactDOM.render(
   <React.StrictMode>
     <ColorModeScript />
-    <ApolloProvider client={client}>
-      <App />
-    </ApolloProvider>
+    <App />
   </React.StrictMode>,
   document.getElementById("root")
 );
