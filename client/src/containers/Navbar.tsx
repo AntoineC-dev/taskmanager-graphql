74199c30496e13e30f47fee4ff213d676a256487
AntoineC-dev <ac.dev@hotmail.com>
2022-02-24 17:00:21 -0400

    Add auth logic to navbar menu

--- client/src/containers/Navbar.tsx
+++ client/src/containers/Navbar.tsx
@@ -1,9 +1,11 @@
-import { ButtonGroup, Divider, HStack, Spacer, StackProps } from "@chakra-ui/react";
+import { Button, ButtonGroup, Divider, HStack, Spacer, StackProps } from "@chakra-ui/react";
 import { ColorModeSwitcher, RouterButton } from "../components";
 import { APP_ROUTES } from "../navigation";
 import { FaHome } from "react-icons/fa";
+import { useIsAuthenticated } from "../hooks";
 
 export const Navbar = (props: StackProps) => {
+  const isAuthenticated = useIsAuthenticated();
   return (
     <HStack {...props}>
       <ButtonGroup variant="ghost" size="sm" flexGrow={1}>
@@ -11,8 +13,17 @@ export const Navbar = (props: StackProps) => {
           Home
         </RouterButton>
         <Spacer />
-        <RouterButton to={APP_ROUTES.register}>Register</RouterButton>
-        <RouterButton to={APP_ROUTES.login}>Login</RouterButton>
+        {isAuthenticated ? (
+          <>
+            <RouterButton to={APP_ROUTES.dashboard}>Dashboard</RouterButton>
+            <Button>Logout</Button>
+          </>
+        ) : (
+          <>
+            <RouterButton to={APP_ROUTES.register}>Register</RouterButton>
+            <RouterButton to={APP_ROUTES.login}>Login</RouterButton>
+          </>
+        )}
       </ButtonGroup>
       <Divider h="6" orientation="vertical" />
       <ColorModeSwitcher />
