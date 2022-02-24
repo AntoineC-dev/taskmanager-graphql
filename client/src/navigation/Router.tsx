78163bd7a44c97adf40d773a087020bff7093b36
AntoineC-dev <ac.dev@hotmail.com>
2022-02-24 17:30:50 -0400

    Define Public & Protected routes

--- client/src/navigation/Router.tsx
+++ client/src/navigation/Router.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes as RouterRoutes, Route } from "react-router-dom";
 import { Layout } from "./Layout";
 import { DashboardPage, HomePage, LoginPage, RegisterPage, VerifyPage } from "../pages";
 import { APP_ROUTES } from "./APP_ROUTES";
+import { ProtectedRoute, PublicRoute } from "../components";
 
 export const Router = () => {
   return (
@@ -9,9 +10,9 @@ export const Router = () => {
       <RouterRoutes>
         <Route path={APP_ROUTES.home} element={<Layout />}>
           <Route index element={<HomePage />} />
-          <Route path={APP_ROUTES.register} element={<RegisterPage />} />
-          <Route path={APP_ROUTES.login} element={<LoginPage />} />
-          <Route path={APP_ROUTES.dashboard} element={<DashboardPage />} />
+          <Route path={APP_ROUTES.register} element={<PublicRoute Element={RegisterPage} />} />
+          <Route path={APP_ROUTES.login} element={<PublicRoute Element={LoginPage} />} />
+          <Route path={APP_ROUTES.dashboard} element={<ProtectedRoute Element={DashboardPage} />} />
         </Route>
         <Route path={APP_ROUTES.verify} element={<VerifyPage />} />
       </RouterRoutes>
