f78b12e7c20fe4be22dd81eaa824f26bc6367138
AntoineC-dev <ac.dev@hotmail.com>
2022-02-24 18:25:57 -0400

    Add SendVerificationEmailModal to LoginPage

--- client/src/pages/LoginPage.tsx
+++ client/src/pages/LoginPage.tsx
@@ -1,6 +1,6 @@
 import { Button, Code, Heading, useToast, VStack } from "@chakra-ui/react";
 import { LoginFormInput, loginFormSchema } from "../validators";
-import { HookFormInput } from "../components";
+import { HookFormInput, SendVerificationEmailModal } from "../components";
 import { useLoginMutation, useResolverForm } from "../hooks";
 import { login } from "../utils";
 
@@ -24,16 +24,19 @@ export const LoginPage = () => {
   };
 
   return (
-    <VStack as="form" maxW="container.sm" w="100%" justifySelf="center" spacing={4} onSubmit={handleSubmit(onSubmit)}>
+    <VStack maxW="container.sm" w="100%" justifySelf="center" spacing={4}>
       <Heading>Login to your Account</Heading>
       <Code fontSize="md" px="2">
         GQLMutation: LOGIN_MUTATION
       </Code>
-      <HookFormInput control={control} name="email" type="email" />
-      <HookFormInput control={control} name="password" type="password" />
-      <Button type="submit" disabled={loading}>
-        Register Now
-      </Button>
+      <VStack as="form" w="100%" onSubmit={handleSubmit(onSubmit)}>
+        <HookFormInput control={control} name="email" type="email" />
+        <HookFormInput control={control} name="password" type="password" />
+        <Button type="submit" disabled={loading}>
+          Register Now
+        </Button>
+      </VStack>
+      <SendVerificationEmailModal />
     </VStack>
   );
 };
