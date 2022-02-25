01b5e51344e36ca42cfa16694f767cad536cb061
AntoineC-dev <ac.dev@hotmail.com>
2022-02-24 20:41:20 -0400

    Create SendPasswordResetCodeEmailModal

---
+++ client/src/components/modals/SendPasswordResetCodeEmailModal.tsx
@@ -0,0 +1,56 @@
+import {
+  Button,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
+  useDisclosure,
+  useToast,
+} from "@chakra-ui/react";
+import { useResolverForm, useSendVerificationEmailLazyQuery } from "../../hooks";
+import { EmailFormInput, emailFormSchema } from "../../validators";
+import { HookFormInput } from "../HookFormInput";
+
+export const SendPasswordResetCodeEmailModal = () => {
+  const { isOpen, onClose, onOpen } = useDisclosure();
+  const { control, handleSubmit, reset } = useResolverForm<EmailFormInput>({
+    schema: emailFormSchema,
+    defaultValues: { email: "" },
+  });
+  const onCloseReset = () => {
+    reset();
+    onClose();
+  };
+  const toast = useToast();
+  const [sendEmail] = useSendVerificationEmailLazyQuery({
+    onCompleted: ({ sendVerificationEmail }) => {
+      toast({ title: sendVerificationEmail, status: "success", isClosable: true });
+      onCloseReset();
+    },
+    onError: (_) => onCloseReset(),
+  });
+  const onSubmit = (variables: EmailFormInput) => sendEmail({ variables });
+  return (
+    <>
+      <Button onClick={onOpen} variant="ghost" size="sm">
+        Forgot your password
+      </Button>
+      <Modal isOpen={isOpen} onClose={onCloseReset}>
+        <ModalOverlay />
+        <ModalContent as="form" onSubmit={handleSubmit(onSubmit)}>
+          <ModalHeader>Send password reset code</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            <HookFormInput control={control} name="email" type="email" />
+          </ModalBody>
+          <ModalFooter>
+            <Button type="submit">Send email</Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+    </>
+  );
+};
--- client/src/components/modals/index.ts
+++ client/src/components/modals/index.ts
@@ -1,1 +1,2 @@
+export * from "./SendPasswordResetCodeEmailModal";
 export * from "./SendVerificationEmailModal";
