6307825200ec5dc07d31d910e4274224b360c1d0
AntoineC-dev <ac.dev@hotmail.com>
2022-02-26 00:49:14 -0400

    Create Menu Component

--- client/src/components/layout/navbar/Menu.tsx
+++ client/src/components/layout/navbar/Menu.tsx
@@ -1,18 +1,36 @@
 import { HamburgerIcon } from "@chakra-ui/icons";
-import { IconButton, Menu as ChakraMenu, MenuButton, MenuGroup, MenuItem, MenuList } from "@chakra-ui/react";
+import { IconButton, Menu as ChakraMenu, MenuButton, MenuItem, MenuList, useToast } from "@chakra-ui/react";
+import { RouterMenuItem } from "../..";
+import { useLogoutMutation } from "../../../hooks";
+import { APP_ROUTES } from "../../../navigation";
+import { logout } from "../../../utils";
 
 export const Menu = () => {
+  const toast = useToast();
+  const [logoutUser, { loading }] = useLogoutMutation({
+    onCompleted: (data) => {
+      logout();
+      toast({ title: data.logout, status: "success", isClosable: true });
+    },
+  });
+  const onLogout = () => logoutUser();
   return (
     <ChakraMenu>
-      <MenuButton as={IconButton} aria-label="Navigation" icon={<HamburgerIcon />} />
+      <MenuButton
+        as={IconButton}
+        aria-label="open navigation menu"
+        size="md"
+        fontSize="lg"
+        variant="ghost"
+        color="current"
+        icon={<HamburgerIcon />}
+      />
       <MenuList>
-        <MenuGroup title="Tasks">
-          <MenuItem>Dashboard</MenuItem>
-          <MenuItem>Archive</MenuItem>
-        </MenuGroup>
-        <MenuGroup title="Profile">
-          <MenuItem>Profile</MenuItem>
-        </MenuGroup>
+        <RouterMenuItem to={APP_ROUTES.dashboard}>Dashboard</RouterMenuItem>
+        <MenuItem>Profile</MenuItem>
+        <MenuItem onClick={onLogout} isDisabled={loading}>
+          Logout
+        </MenuItem>
       </MenuList>
     </ChakraMenu>
   );
