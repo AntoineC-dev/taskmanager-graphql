f85a12e020502eac277668ccb4e84fb58c4bad93
AntoineC-dev <ac.dev@hotmail.com>
2022-02-26 03:16:48 -0400

    Switch string to SuccessMessage return data

--- client/src/graphql/auth/auth.mutations.ts
+++ client/src/graphql/auth/auth.mutations.ts
@@ -1,15 +1,18 @@
 import { ApolloCache, DefaultContext, gql, MutationHookOptions } from "@apollo/client";
-import { AuthPayload } from "../../models";
+import { AuthPayload, SuccessMessage } from "../../models";
 import { RegisterFormInput } from "../../validators";
 
 export type UseMutationOptions<T, U> = Omit<MutationHookOptions<T, U, DefaultContext, ApolloCache<any>>, "variables">;
 
 // REGISTER_MUTATION
-export type RegisterData = { register: string };
+export type RegisterData = { register: SuccessMessage };
 export type RegisterVariables = Omit<RegisterFormInput, "passwordConfirmation">;
 export const REGISTER_MUTATION = gql`
   mutation Register($username: NonEmptyString!, $email: EmailAddress!, $password: Password!) {
-    register(username: $username, email: $email, password: $password)
+    register(username: $username, email: $email, password: $password) {
+      title
+      description
+    }
   }
 `;
 
@@ -20,24 +23,33 @@ export const LOGIN_MUTATION = gql`
     login(email: $email, password: $password) {
       accessToken
       refreshToken
-      message
+      message {
+        title
+        description
+      }
     }
   }
 `;
 
 // LOGOUT_MUTATION
-export type LogoutData = { logout: string };
+export type LogoutData = { logout: SuccessMessage };
 export const LOGOUT_MUTATION = gql`
   mutation Logout {
-    logout
+    logout {
+      title
+      description
+    }
   }
 `;
 
 // RESET_PASSWORD_MUTATION
 export type ResetPasswordVariables = { id: string; password: string; passwordResetCode: string };
-export type ResetPasswordData = { resetPassword: string };
+export type ResetPasswordData = { resetPassword: SuccessMessage };
 export const RESET_PASSWORD_MUTATION = gql`
   mutation ResetPassword($id: String!, $password: Password!, $passwordResetCode: String!) {
-    resetPassword(id: $id, password: $password, passwordResetCode: $passwordResetCode)
+    resetPassword(id: $id, password: $password, passwordResetCode: $passwordResetCode) {
+      title
+      description
+    }
   }
 `;
--- client/src/graphql/auth/auth.queries.ts
+++ client/src/graphql/auth/auth.queries.ts
@@ -1,30 +1,40 @@
 import { gql, QueryHookOptions } from "@apollo/client";
+import { SuccessMessage } from "../../models";
 
 export type UseQueryOptionsOmitVariables<T, U> = Omit<QueryHookOptions<T, U>, "variables">;
 
 // VERIFY_QUERY
 export type VerifyVariables = { id: string; verificationCode: string };
-export type VerifyData = { verify: string };
+export type VerifyData = { verify: SuccessMessage };
 export const VERIFY_QUERY = gql`
   query ($id: String!, $verificationCode: String!) {
-    verify(id: $id, verificationCode: $verificationCode)
+    verify(id: $id, verificationCode: $verificationCode) {
+      title
+      description
+    }
   }
 `;
 
 // VERIFICATION_EMAIL_QUERY
 export type VericationEmailVariables = { email: string };
-export type VericationEmailData = { verificationEmail: string };
+export type VericationEmailData = { verificationEmail: SuccessMessage };
 export const VERIFICATION_EMAIL_QUERY = gql`
   query ($email: String!) {
-    verificationEmail(email: $email)
+    verificationEmail(email: $email) {
+      title
+      description
+    }
   }
 `;
 
 // RESET_CODE_EMAIL_QUERY
 export type resetCodeEmailVariables = { email: string };
-export type resetCodeEmailData = { resetCodeEmail: string };
+export type resetCodeEmailData = { resetCodeEmail: SuccessMessage };
 export const RESET_CODE_EMAIL_QUERY = gql`
   query ($email: String!) {
-    resetCodeEmail(email: $email)
+    resetCodeEmail(email: $email) {
+      title
+      description
+    }
   }
 `;
