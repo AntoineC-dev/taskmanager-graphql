e08c738f2b52af2ea7da07025518973f9620c8b1
Antoine Cheminat <ac.dev@hotmail.com>
2022-03-09 22:58:48 -0400

    Update DashboardPage

--- client/src/pages/DashboardPage.tsx
+++ client/src/pages/DashboardPage.tsx
@@ -1,22 +1,36 @@
-import { Center, Heading, Spinner, Text, VStack } from "@chakra-ui/react";
+import { Center, Heading, IconButton, Spinner, Tooltip } from "@chakra-ui/react";
+import { PlusSquareIcon } from "@chakra-ui/icons";
 import { useMeQuery } from "../hooks";
+import { Dashboard } from "../components";
 
 export const DashboardPage = () => {
   const { data, loading } = useMeQuery();
-  return (
+  if (loading)
+    return (
+      <Center h="100%">
+        <Spinner size="xl" thickness="4px" speed="0.8s" />
+      </Center>
+    );
+  return data ? (
+    <>
+      <Dashboard username={data.me.username} tasks={data.me.tasks} />
+      <Tooltip label="Create a new task">
+        <IconButton
+          aria-label="create new task"
+          size="md"
+          fontSize="lg"
+          variant="solid"
+          color="current"
+          icon={<PlusSquareIcon />}
+          pos="absolute"
+          right="4"
+          bottom="4"
+        />
+      </Tooltip>
+    </>
+  ) : (
     <Center h="100%">
-      <VStack spacing={8}>
-        {loading ? (
-          <Spinner size="xl" thickness="4px" speed="0.8s" />
-        ) : (
-          <>
-            <Heading>Dashboard</Heading>
-            <Text>{`Id: ${data?.me.id}`}</Text>
-            <Text>{`Username: ${data?.me.username}`}</Text>
-            <Text>{`Email: ${data?.me.email}`}</Text>
-          </>
-        )}
-      </VStack>
+      <Heading>We could not access the server... Please contact the support!</Heading>
     </Center>
   );
 };
