2f267f3dd8078597d8316cbb13fbf38a6f235628
Antoine Cheminat <ac.dev@hotmail.com>
2022-03-09 22:58:24 -0400

    Update meQuery to include tasks in results

--- client/src/graphql/user/user.queries.ts
+++ client/src/graphql/user/user.queries.ts
@@ -2,13 +2,19 @@ import { gql } from "@apollo/client";
 import { User } from "../../models";
 
 // ME_QUERY
-export type MeData = { me: Pick<User, "id" | "email" | "username"> };
+export type MeData = { me: User };
 export const ME_QUERY = gql`
   query {
     me {
       id
       username
       email
+      tasks {
+        id
+        title
+        description
+        completed
+      }
     }
   }
 `;
