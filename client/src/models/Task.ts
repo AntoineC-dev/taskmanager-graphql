c9599ce02767e8cdf6ecb86e572ebca09192f525
Antoine Cheminat <ac.dev@hotmail.com>
2022-03-14 20:33:16 -0400

    create CoreTasksFields fragment

--- client/src/models/Task.ts
+++ client/src/models/Task.ts
@@ -1,5 +1,15 @@
+import { gql } from "@apollo/client";
+
 export interface Task {
   id: string;
   title: string;
   completed: boolean;
 }
+
+export const CORE_TASK_FIELDS = gql`
+  fragment CoreTaskFields on Task {
+    id
+    title
+    completed
+  }
+`;
