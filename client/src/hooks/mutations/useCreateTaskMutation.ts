f871cec8472012e1a9aaa0736fda496829c35bd6
Antoine Cheminat <ac.dev@hotmail.com>
2022-03-14 21:06:41 -0400

    Extract update option from Component to useCreateTaskMutation hook

--- client/src/hooks/mutations/useCreateTaskMutation.ts
+++ client/src/hooks/mutations/useCreateTaskMutation.ts
@@ -1,7 +1,23 @@
 import { MutationHookOptions, useMutation } from "@apollo/client";
 import { CreateTaskData, CREATE_TASK_MUTATION } from "../../graphql";
+import { CORE_TASK_FIELDS } from "../../models";
 import { CreateTaskInput } from "../../validators";
 
-export function useCreateTaskMutation(options?: MutationHookOptions<CreateTaskData, CreateTaskInput>) {
-  return useMutation<CreateTaskData, CreateTaskInput>(CREATE_TASK_MUTATION, options);
+export function useCreateTaskMutation(options?: Omit<MutationHookOptions<CreateTaskData, CreateTaskInput>, "update">) {
+  return useMutation<CreateTaskData, CreateTaskInput>(CREATE_TASK_MUTATION, {
+    update(cache, { data }) {
+      cache.modify({
+        fields: {
+          tasks(existingTasks = []) {
+            const newTaskRef = cache.writeFragment({
+              data: data?.createTask,
+              fragment: CORE_TASK_FIELDS,
+            });
+            return [...existingTasks, newTaskRef];
+          },
+        },
+      });
+    },
+    ...(options && options),
+  });
 }
