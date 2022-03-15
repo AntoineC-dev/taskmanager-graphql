3c2a53577aa295ee83960fe4ea8e5f386393269b
Antoine Cheminat <ac.dev@hotmail.com>
2022-03-14 20:31:00 -0400

    Update MeQuery

--- client/src/graphql/user/user.queries.ts
+++ client/src/graphql/user/user.queries.ts
@@ -2,18 +2,13 @@ import { gql } from "@apollo/client";
 import { User } from "../../models";
 
 // ME_QUERY
-export type MeData = { me: User };
+export type MeData = { me: Omit<User, "tasks"> };
 export const ME_QUERY = gql`
-  query {
+  query GetUser {
     me {
       id
       username
       email
-      tasks {
-        id
-        title
-        completed
-      }
     }
   }
 `;
